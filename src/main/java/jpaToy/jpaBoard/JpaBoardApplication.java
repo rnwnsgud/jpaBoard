1d93602221e31aa529e6e440aa8b0933512ee9c2
rnwnsgud <rnwnsgud90@naver.com>
2022-05-03 17:37:14 +0900

    Initial commit

---
+++ src/main/java/jpaToy/jpaBoard/JpaBoardApplication.java
@@ -0,0 +1,13 @@
+package jpaToy.jpaBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpaToy/jpaBoard/JpaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package jpaToy.jpaBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
